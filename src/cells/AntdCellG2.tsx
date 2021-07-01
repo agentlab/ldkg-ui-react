4a0cd23c3b0b9cd4f89a9231a016b499a6856dee
Artyom <artkrygin@yandex.ru>
2021-07-01 10:03:42 +0300

    [ADD] chart images

--- src/cells/AntdCellG2.tsx
+++ src/cells/AntdCellG2.tsx
@@ -3,15 +3,21 @@ import React from 'react';
 import { uiTypeIs, rankWith, RankedTester } from '../testers';
 import { withStoreToCellProps } from '../util/ContextToProps';
 import { Image } from 'antd';
+import chart1 from './images/chart_1.png';
+import chart2 from './images/chart_2.png';
+import chart3 from './images/chart_3.png';
+import chart4 from './images/chart_4.png';
+
+const template: any = {
+  'https://www.wildberries.ru/catalog/15570386/detail.aspx': chart1,
+  'https://www.wildberries.ru/catalog/15622789/detail.aspx': chart2,
+  'https://www.wildberries.ru/catalog/16170086/detail.aspx': chart3,
+  'https://www.wildberries.ru/catalog/18247707/detail.aspx': chart4,
+};
 
 export const AntdCellG2 = (props: any) => {
-  return (
-    <Image
-      src={
-        'https://lh3.googleusercontent.com/proxy/uz_yL8cj6Fu63l9018tT6gJhcdFloEGfiIOV5AM2uR0pjJ4q05uTnOsXtEJrGErxrc_35_rb_-_jqlXF0bNbVrJr01nb2CyZn0qx5fYdBW6D'
-      }
-    />
-  );
+  console.log('PROPS', props);
+  return <Image src={template[props.data['@id']]} />;
 };
 
 /**
