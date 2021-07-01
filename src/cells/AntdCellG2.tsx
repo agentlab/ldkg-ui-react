1235c208ca7553152eb8aba67f36879795d0c416
Artyom <artkrygin@yandex.ru>
2021-07-01 10:54:20 +0300

    [FIX] charts images

--- src/cells/AntdCellG2.tsx
+++ src/cells/AntdCellG2.tsx
@@ -3,18 +3,13 @@ import React from 'react';
 import { uiTypeIs, rankWith, RankedTester } from '../testers';
 import { withStoreToCellProps } from '../util/ContextToProps';
 import { Image } from 'antd';
-import chart1 from './images/chart_1.png';
-import chart2 from './images/chart_2.png';
-import chart3 from './images/chart_3.png';
-import chart4 from './images/chart_4.png';
 
 const template: any = {
-  'https://www.wildberries.ru/catalog/15570386/detail.aspx': chart1,
-  'https://www.wildberries.ru/catalog/15622789/detail.aspx': chart2,
-  'https://www.wildberries.ru/catalog/16170086/detail.aspx': chart3,
-  'https://www.wildberries.ru/catalog/18247707/detail.aspx': chart4,
+  'https://www.wildberries.ru/catalog/15570386/detail.aspx': '/img/chart_1.png',
+  'https://www.wildberries.ru/catalog/15622789/detail.aspx': '/img/chart_2.png',
+  'https://www.wildberries.ru/catalog/16170086/detail.aspx': '/img/chart_3.png',
+  'https://www.wildberries.ru/catalog/18247707/detail.aspx': '/img/chart_4.png',
 };
-
 export const AntdCellG2 = (props: any) => {
   console.log('PROPS', props);
   return <Image src={template[props.data['@id']]} />;
