9de55c214bc723e797f75a4067b287901d810ea9
Alexey Ivanov <amivanoff@gmail.com>
2021-10-12 20:05:43 +0300

    [refactor] Image size and data

--- src/controls/AntdImageControl.tsx
+++ src/controls/AntdImageControl.tsx
@@ -5,7 +5,13 @@ import { withStoreToControlProps } from '../util/ContextToProps';
 
 export const AntdImageControl = (props: any): JSX.Element => {
   const { uiOptions, data } = props;
-  return <Image height={'100%'} src={Array.isArray(data) ? data[0] || '' : ''} fallback={uiOptions.fallback} />;
+  return (
+    <Image
+      height={'80%'}
+      src={Array.isArray(data) ? data[0] || '' : data && typeof data === 'string' ? data : ''}
+      fallback={uiOptions.fallback}
+    />
+  );
 };
 
 export const antdImageControlTester: RankedTester = rankWith(3, uiTypeIs('aldkg:Image'));
